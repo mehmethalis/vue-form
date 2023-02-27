9cf6fb178de266d5df0da1b94a63c86a1fb732cb
mehmethalis <mehmethaliscicek@gmail.com>
2023-02-27 21:11:40 +0300

    form input methods

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,63 +1,107 @@
 <template>
-    <div>
+  <div>
+    <h1>Form</h1>
+    <div class="form">
+      <form action="">
+        <label for="">Name</label><br />
+        <input type="text" v-model="form.name" placeholder="Name" /><br />
+        <label for="">Surname</label><br />
+        <input type="text" v-model="form.lastname" placeholder="Surname" /><br />
+        <label for="email">Email</label><br />
+        <input type="text" v-model="form.email" placeholder="Email" /><br />
+        <label for="phone">Phone</label><br />
+        <input
+          type="tel"
+          v-model="form.phone"
+          id="phone"
+          name="phone"
+          pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"
+          placeholder="Your number"
+        /><br />
+        <label for="">City</label><br />
+        <input type="text" v-model="form.city" placeholder="City" /><br />
+        <label for="">District</label><br />
+        <input type="text" v-model="form.district" placeholder="District" />
 
-        <h1>Form</h1>
-        <div>
-            <form action="">
-                <label for="">Name</label><br>
-                <input type="text" placeholder="Name"><br>
-                <label for="">Surname</label><br>
-                <input type="text" placeholder="Surname"><br>
-                <label for="phone">Enter your phone number</label><br>
-                <input type="tel" id="phone" name="phone" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"
-                    placeholder="Your number"><br>
-                <label for="">City</label><br>
-                <input type="text" placeholder="City"><br>
-                <label for="">District</label><br>
-                <input type="text" placeholder="District">
-            </form>
+        <div class="checkbox">
+          <input type="checkbox" v-model="form.isAgree" id="policy" name="policy" value="true" />
+          <label for="policy">I agree to <a href="#" @click="setShow">Privacy Policy</a></label>
         </div>
-        <Popup v-if="isShow" @setShow="setShow" />
-        <a href="#" @click="setShow">Open</a>
+        <button type="button" class="btn" @click="submit">Submit</button>
+      </form>
     </div>
+    <Popup v-if="isShow" @setShow="setShow" />
+  </div>
 </template>
-   
-<script>
-import Popup from './Popup.vue'
 
+<script>
+import Popup from "./Popup.vue";
 
 export default {
-    name: 'Form',
-    data() {
-        return {
-            isShow: false
-        }
+  name: "Form",
+  data() {
+    return {
+      isShow: false,
+      form: {
+        name: "",
+        lastname: "",
+        phone: "",
+        email: "",
+        city: "",
+        district: "",
+        isAgree: false,
+      },
+    };
+  },
+  methods: {
+    setShow() {
+      this.isShow = !this.isShow;
     },
-    methods: {
-        setShow() {
-            this.isShow = !this.isShow
-        }
+    submit() {
+      console.log(this.form);
     },
-    components: {
-        Popup,
-    },
-    props: {
-
-    }
-}
+  },
+  components: {
+    Popup,
+  },
+  props: {},
+};
 </script>
-   
-<style>
+
+<style scoped>
+.form {
+  width: 100%;
+}
+.btn {
+  width: 100%;
+  padding: 15px 25px;
+  border: none;
+  border-radius: 15px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  transition: all ease-in-out 0.3s;
+}
+.checkbox {
+  display: flex;
+  padding: 10px 5px;
+  gap: 10px;
+}
 input {
-    width: 210px;
-    padding: 7px;
-    border-radius: 5px;
-    border: solid 2px black;
+  padding: 7px;
+  border-radius: 5px;
+  border: solid 2px black;
+  outline: none;
+  transition: all ease-in-out 0.3s;
+}
+
+input:focus {
+  border-color: gray;
 }
 
 label {
-    font-weight: 700;
-    font-family: 'Poppins', sans-serif;
+  font-weight: 700;
+  font-family: "Poppins", sans-serif;
 }
 </style>
-   
--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -1,78 +1,82 @@
 <template>
-    <div class="modal-backdrop">
-        <div class="modal-popup">
-            <div class="modal-header">
-                <h1>Policy</h1>
-                <button @click="$emit('setShow')" class="btn">X</button>
-            </div>
-            <div class="modal-body">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam beatae maiores, minima
-                possimus vero exercitationem voluptates iusto libero hic culpa, ex deleniti eaque nobis quisquam illo unde
-                porro ut atque neque repudiandae. Maxime ipsum aspernatur esse dolorem facilis blanditiis, reiciendis ullam
-                at animi, dolor sint est nemo officiis sequi fugit laborum nostrum! Tempora enim vel, a excepturi odio
-                ratione eaque nulla, esse unde similique cumque sapiente architecto, libero dolore est voluptatum. Expedita
-                ea ullam amet repellat numquam minus eos fugit reiciendis impedit ad natus incidunt eius, deleniti totam
-                voluptatem ex sed. Atque veritatis reprehenderit quia labore, incidunt expedita reiciendis fuga rem neque
-                vel, perferendis eius ullam deserunt praesentium blanditiis. Quo fuga dolore unde officia debitis provident
-                quia inventore voluptates consequuntur, ipsum eveniet assumenda explicabo amet quam! Magni consequuntur
-                obcaecati accusamus fuga minus nulla? Autem pariatur dolores quia debitis voluptatum veritatis explicabo
-                porro dolorum corporis sequi, dicta magni incidunt nisi nostrum aperiam, sint magnam non! Porro sequi beatae
-                voluptates ab eaque incidunt odio! Ex excepturi a rem odit quidem doloribus eos officiis velit. Perferendis
-                voluptates modi debitis iste alias labore sunt. Doloremque vel laborum ipsa tempora recusandae eum expedita
-                officiis provident molestiae nobis unde mollitia commodi quos ut, optio modi aliquid perferendis voluptatem,
-                debitis illum odit. Et dignissimos cum repudiandae sunt. Mollitia, voluptate illo tempore suscipit obcaecati
-                quam excepturi culpa deserunt, minima iure consequuntur. Neque, sequi quibusdam sit commodi saepe sapiente!
-                Laudantium laborum nam quam blanditiis nostrum ea esse suscipit similique non ratione nulla quidem,
-                obcaecati ullam adipisci ad numquam corrupti!</div>
-        </div>
+  <div class="modal-backdrop">
+    <div class="modal-popup">
+      <div class="modal-header">
+        <h1>Policy</h1>
+        <button @click="$emit('setShow')" class="btn">X</button>
+      </div>
+      <div class="modal-body">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam beatae maiores, minima
+        possimus vero exercitationem voluptates iusto libero hic culpa, ex deleniti eaque nobis
+        quisquam illo unde porro ut atque neque repudiandae. Maxime ipsum aspernatur esse dolorem
+        facilis blanditiis, reiciendis ullam at animi, dolor sint est nemo officiis sequi fugit
+        laborum nostrum! Tempora enim vel, a excepturi odio ratione eaque nulla, esse unde similique
+        cumque sapiente architecto, libero dolore est voluptatum. Expedita ea ullam amet repellat
+        numquam minus eos fugit reiciendis impedit ad natus incidunt eius, deleniti totam voluptatem
+        ex sed. Atque veritatis reprehenderit quia labore, incidunt expedita reiciendis fuga rem
+        neque vel, perferendis eius ullam deserunt praesentium blanditiis. Quo fuga dolore unde
+        officia debitis provident quia inventore voluptates consequuntur, ipsum eveniet assumenda
+        explicabo amet quam! Magni consequuntur obcaecati accusamus fuga minus nulla? Autem pariatur
+        dolores quia debitis voluptatum veritatis explicabo porro dolorum corporis sequi, dicta
+        magni incidunt nisi nostrum aperiam, sint magnam non! Porro sequi beatae voluptates ab eaque
+        incidunt odio! Ex excepturi a rem odit quidem doloribus eos officiis velit. Perferendis
+        voluptates modi debitis iste alias labore sunt. Doloremque vel laborum ipsa tempora
+        recusandae eum expedita officiis provident molestiae nobis unde mollitia commodi quos ut,
+        optio modi aliquid perferendis voluptatem, debitis illum odit. Et dignissimos cum
+        repudiandae sunt. Mollitia, voluptate illo tempore suscipit obcaecati quam excepturi culpa
+        deserunt, minima iure consequuntur. Neque, sequi quibusdam sit commodi saepe sapiente!
+        Laudantium laborum nam quam blanditiis nostrum ea esse suscipit similique non ratione nulla
+        quidem, obcaecati ullam adipisci ad numquam corrupti!
+      </div>
     </div>
+  </div>
 </template>
-   
+
 <script>
 export default {
-    name: 'Popup',
-    props: {
-
-    }
-}
+  name: "Popup",
+  props: {},
+};
 </script>
-   
+
 <style>
 .modal-backdrop {
-    position: absolute;
-    height: 100vh;
-    background: rgba(0, 0, 0, 0.2);
-    z-index: 2;
-    top: 0;
-    left: 0;
-    width: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
+  position: absolute;
+  height: 100vh;
+  background: rgba(0, 0, 0, 0.2);
+  z-index: 2;
+  top: 0;
+  left: 0;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 
 .modal-popup {
-    background: #fff;
-    width: 500px;
-    border-radius: 10px;
-    padding: 20px;
+  background: #fff;
+  width: 500px;
+  border-radius: 10px;
+  padding: 20px;
 }
 
 .modal-header {
-    display: flex;
-    justify-content: space-between;
+  display: flex;
+  justify-content: space-between;
 }
 
 .modal-body {
-    height: 500px;
-    overflow-y: scroll;
-    overflow-x: hidden;
-    padding: 20px;
+  height: 500px;
+  overflow-y: scroll;
+  overflow-x: hidden;
+  padding: 20px;
 }
 
 .btn {
-    border-radius: 15px;
-    height: 21px;
-    width: 20px;
+  border-radius: 5px;
+  border: none;
+  cursor: pointer;
+  height: 35px;
+  width: 35px;
 }
 </style>
-   
